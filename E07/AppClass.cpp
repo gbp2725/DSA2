83a13f63ef88d7aa3efd2ae3e8c31056850e17aa
Garrett Paradis <56126763+gbp2725@users.noreply.github.com>
2021-03-10 23:42:21 -0500

    Add files via upload
    E07 Deliverable

---
+++ E07/AppClass.cpp
@@ -0,0 +1,56 @@
+#include "AppClass.h"
+void Application::InitVariables(void)
+{
+	//init the mesh
+	m_pMesh = new MyMesh();
+	//m_pMesh->GenerateCube(1.0f, C_WHITE);
+	m_pMesh->GenerateCone(2.0f, 5.0f, 3, C_WHITE);
+}
+void Application::Update(void)
+{
+	//Update the system so it knows how much time has passed since the last call
+	m_pSystem->Update();
+
+	//Is the arcball active?
+	ArcBall();
+
+	//Is the first person camera active?
+	CameraRotation();
+}
+void Application::Display(void)
+{
+	// Clear the screen
+	ClearScreen();
+
+	matrix4 m4View = m_pCameraMngr->GetViewMatrix();
+	matrix4 m4Projection = m_pCameraMngr->GetProjectionMatrix();
+
+	quaternion resultQuat = m_qOrientation * glm::angleAxis(glm::radians(1.0f), m_v3Rotation);
+	quaternion inverse = glm::inverse(m_qOrientation);
+	quaternion finalQuat = inverse * resultQuat;
+	m_qOrientation += finalQuat;
+
+	m_pMesh->Render(m4Projection, m4View, ToMatrix4(finalQuat));
+	
+	// draw a skybox
+	m_pMeshMngr->AddSkyboxToRenderList();
+	
+	//render list call
+	m_uRenderCallCount = m_pMeshMngr->Render();
+
+	//clear the render list
+	m_pMeshMngr->ClearRenderList();
+	
+	//draw gui
+	DrawGUI();
+	
+	//end the current frame (internally swaps the front and back buffers)
+	m_pWindow->display();
+}
+void Application::Release(void)
+{
+	SafeDelete(m_pMesh);
+
+	//release GUI
+	ShutdownGUI();
+}
